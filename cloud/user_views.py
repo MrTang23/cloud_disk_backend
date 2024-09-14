f4ced7bf68a2d89c0693ab76588042f731efae64
Amos Tang <amostang23@icloud.com>
2024-09-14 12:05:57 +0800

    fix(user view): update login func

--- cloud/user_views.py
+++ cloud/user_views.py
@@ -9,6 +9,7 @@ from django.db.models import Q
 from rest_framework import status
 from django.template import loader
 
+from cloud.models import TempToken
 from cloud_disk_backend import global_function, settings
 
 from cloud import models as cloud_models
@@ -56,12 +57,7 @@ def register(request):
 # 登陆
 def login(request):
     data = json.loads(request.body)
-    # 生成包含大小写字母和数字的字符集合并进行md5加密作为token
-    characters = string.ascii_letters + string.digits
-    token = ''
-    for item in range(3):
-        token_before = ''.join(random.choice(characters) for _ in range(10))
-        token = token + global_function.to_md5(token_before)
+
     # method=1时为账号密码登陆，2为邮箱以及验证码登陆
     if data.get('method') == '1':
         username = data.get('username')
@@ -72,13 +68,12 @@ def login(request):
             if global_function.to_md5(password) == user_password:
                 current_time = timezone.now().strftime("%Y-%m-%d %H:%M:%S")
                 # token表中存储信息
-                cloud_models.Token.objects.create(username=username, token=token,
-                                                  start_time=current_time)
+                temp_token = TempToken.objects.create(username=username)
                 # 更新user表中的最后一次登陆时间以及ip
                 cloud_models.User.objects.filter(username=username, password=user_password).update(
                     last_login_time=current_time, last_login_ip=global_function.get_ip(request))
                 return global_function.json_response({
-                    'token': token,
+                    'token': temp_token.token,
                     'username': username,
                     'email': user_queryset.email,
                     'uuid': user_queryset.uuid
@@ -87,16 +82,3 @@ def login(request):
                 return global_function.json_response('', '密码错误', status.HTTP_405_METHOD_NOT_ALLOWED)
         else:
             return global_function.json_response('', '用户名不存在', status.HTTP_405_METHOD_NOT_ALLOWED)
-
-
-def check_user_status(request):
-    check_result = global_function.check_token(request)
-    if check_result:
-        user_queryset = cloud_models.User.objects.get(username=check_result)
-        return global_function.json_response({
-            'username': user_queryset.username,
-            'email': user_queryset.email,
-            'uuid': user_queryset.uuid
-        }, '登陆成功', status.HTTP_200_OK)
-    else:
-        return global_function.json_response('', 'token已过期或不存在，请重新登陆', status.HTTP_403_FORBIDDEN)
