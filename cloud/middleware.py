a0fd656ec6e15168abf87d55ec57e29f02bac99c
Amos Tang <amostang23@icloud.com>
2024-09-14 11:47:49 +0800

    refactor(token): update temp token func

--- cloud/middleware.py
+++ cloud/middleware.py
@@ -1,56 +1,24 @@
 # 创建中间件，进行鉴权
-import json
-from datetime import datetime
-from hashlib import md5
-
-from django.utils import timezone
 from rest_framework import status
-
+from cloud.models import TempToken
 from cloud_disk_backend import global_function
-
-from cloud import models as cloud_models
-
-
-# md5加密
-def to_md5(string_name):
-    md5_object = md5()
-    md5_object.update(string_name.encode(encoding='utf-8'))
-    return md5_object.hexdigest()
 
 
 # 验证请求头中token是否有效
-def check_token(request):
-    token = request.META.get('HTTP_AMOS_CLOUD_TOKEN')
-    # 验证token时同时对md5加密后的用户名和数据库中token对应的用户名匹配检查
-    username_md5 = request.META.get('HTTP_AMOS_CLOUD_USER')
-    if cloud_models.Token.objects.filter(token=token).exists():
-        token_queryset = cloud_models.Token.objects.get(token=token)
-        if to_md5(token_queryset.username) == username_md5:
-            # 计算时间差
-            current_time = timezone.now().strftime("%Y-%m-%d %H:%M:%S")
-            current_time = datetime.strptime(current_time, "%Y-%m-%d %H:%M:%S")
-            token_start_time = datetime.strptime(token_queryset.start_time, "%Y-%m-%d %H:%M:%S")
-            time_difference = (current_time - token_start_time).total_seconds() / 3600
-            # 时间差大于7*24着返回false并删除该token
-            if time_difference > 7 * 24:
-                cloud_models.Token.objects.filter(token=token).delete()
-                return False
-            # token通过则返回用户名
-            return token_queryset.username
+def check_token_validity(request):
+    token_str = request.META.get('HTTP_AMOS_CLOUD_TOKEN')
+    try:
+        # 获取 Token 实例
+        token = TempToken.objects.get(token=token_str)
+
+        # 检查 token 是否有效
+        if token.is_valid():
+            # 返回用户名
+            return token.user.username
         else:
-            return False
-    else:
-        return False
-
-
-def preprocess_request(request):
-    # 在这里对请求执行操作
-    data = json.loads(request.body)
-    check_result = check_token(request)
-    if check_result:
-        print(data)
-    else:
-        return global_function.json_response('', 'token已过期或不存在，请重新登陆', status.HTTP_401_UNAUTHORIZED)
+            return None
+    except TempToken.DoesNotExist:
+        return None
 
 
 class CustomMiddleware:
@@ -59,11 +27,10 @@ class CustomMiddleware:
 
     def __call__(self, request):
         # 在视图处理请求之前执行操作
-        data = json.loads(request.body)
-        check_result = check_token(request)
+        check_result = check_token_validity(request)
         if check_result:
             # 调用视图（处理请求）
-            response = self.get_response(data, check_result)
+            response = self.get_response(request, check_result)
             # 如果需要，也可以在此处对响应进行处理
             return response
         else:
--- cloud/models.py
+++ cloud/models.py
@@ -1,3 +1,7 @@
+import secrets
+import uuid
+from django.utils import timezone
+from datetime import timedelta
 from django.db import models
 
 
@@ -5,8 +9,8 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(verbose_name='user_name', max_length=20, unique=True)
     password = models.CharField(verbose_name='password', max_length=40)
-    email = models.EmailField(verbose_name='email', max_length=50, default='example@domain.com')
-    uuid = models.CharField(verbose_name='id', max_length=18, primary_key=True, unique=True)
+    email = models.EmailField(verbose_name='email', max_length=255, default='example@domain.com')
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     register_time = models.CharField(verbose_name='register_time', max_length=20, default='2024-03-15 08:30:00')
     last_login_ip = models.CharField(verbose_name='ip', max_length=18)
     last_login_time = models.CharField(verbose_name='last_login_time', max_length=20, default='2024-03-15 08:30:00')
@@ -16,9 +20,21 @@ class User(models.Model):
 
 class FileManage(models.Model):
     username = models.CharField(verbose_name='user_name', max_length=20, unique=False)
-    file_name = models.CharField(verbose_name='file_name',max_length=50)
+    file_name = models.CharField(verbose_name='file_name', max_length=50)
 
-class Token(models.Model):
-    token = models.CharField(verbose_name='token', max_length=100, unique=True, primary_key=True)
-    username = models.CharField(verbose_name='user_name', max_length=20, unique=False)
-    start_time = models.CharField(verbose_name='start_time', max_length=20, default='2024-03-15 08:30:00')
+
+class TempToken(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=64, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField()
+
+    def save(self, *args, **kwargs):
+        if not self.token:
+            self.token = secrets.token_hex(32)  # 生成一个64字符的临时token
+        if not self.expires_at:
+            self.expires_at = timezone.now() + timedelta(hours=7 * 24)  # 1小时后过期
+        super().save(*args, **kwargs)
+
+    def is_valid(self):
+        return timezone.now() < self.expires_at
